398cdab1387bb1f5a49f28b61e699f2bbdc21646
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2021-12-27 18:52:43 -0300

    bug fixed

--- src/database/db.ts
+++ src/database/db.ts
@@ -21,10 +21,16 @@ export class conectionDatabase {
     querySelectByEmailAndPassword(email: string, password: string, req: Request, res: any): any {
         this.dbConnection().query(`select * from user_account where email = '${email}' and password = '${password}'`).then((result) => {
             if (result.rowCount > 0) {
-                
+                const tokenAccount = jwt.sign(
+                        { userid: result.rows[0].password },
+                        process.env.TOKEN_COMPARE,
+                        {
+                          expiresIn: 60 * 60,
+                        }
+                      );
                 return res.json({
                     user: result.rows[0],
-                    token: '',
+                    token: tokenAccount,
                     login: true
                 })
             } else {
